756c5835b45bb3e28392ec3b9f39cd9877e24658
Jeremy <jembyfowler@gmail.com>
2023-10-19 14:45:26 -0600

    jeremy rules

--- server/src/models/Bird.js
+++ server/src/models/Bird.js
@@ -1,13 +1,15 @@
 import { Schema } from "mongoose";
 
 export const BirdSchema = new Schema({
-  name: {type: String, required: true, maxlength: 50, minlength: 1},
-  imgUrl: {type: String, default: "https://www.mvnews.org/wp-content/uploads/2020/01/drird.jpg", maxlength: 500},
-  location: {type: String, default: "My backyard.", minlength: 1, maxlength: 500},
-  canFly: {type: Boolean, default: true},
-  isBird: {type: Boolean, default: false},
-  creatorId: {type: Schema.Types.ObjectId, required: true, ref: "Account"}
-}, { timestamps: true, toJSON: { virtuals: true }})
+  name: { type: String, required: true, maxlength: 50, minlength: 1 },
+  imgUrl: { type: String, default: "https://www.mvnews.org/wp-content/uploads/2020/01/drird.jpg", maxlength: 500 },
+  location: { type: String, default: "My backyard.", minlength: 1, maxlength: 500 },
+  canFly: { type: Boolean, default: true },
+  isBird: { type: Boolean, default: false },
+  creatorId: { type: Schema.Types.ObjectId, required: true, ref: "Account" }
+}, { timestamps: true, toJSON: { virtuals: true } })
+
+// NOTE jeremy is a rad dude 🎸🎸🎸🎸🎸🎸🎸🎸🎸🎸
 
 BirdSchema.virtual("creator", {
   localField: "creatorId",
